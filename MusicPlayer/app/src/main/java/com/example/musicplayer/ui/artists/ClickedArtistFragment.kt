4e2ee0ef9548ef3dcc17cfba4abc7d8715024a92
MrRevalis <dominik.revalis@gmail.com>
2021-06-21 16:45:37 +0200

    Favorite icon in artist view

--- MusicPlayer/app/src/main/java/com/example/musicplayer/ui/artists/ClickedArtistFragment.kt
+++ MusicPlayer/app/src/main/java/com/example/musicplayer/ui/artists/ClickedArtistFragment.kt
@@ -7,8 +7,11 @@ import android.view.ViewAnimationUtils
 import android.view.ViewGroup
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
+import android.widget.Toast
 import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.toDrawable
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
@@ -18,6 +21,7 @@ import com.example.musicplayer.databinding.FragmentClickedArtistBinding
 import com.example.musicplayer.music.MusicStore
 import com.example.musicplayer.player.state.QueueConstructor
 import com.example.musicplayer.utils.PreferencesManager
+import com.example.musicplayer.viewmodels.FavoriteViewModel
 import com.example.musicplayer.viewmodels.PlayerViewModel
 import com.google.android.material.appbar.AppBarLayout.OnOffsetChangedListener
 import com.google.android.material.appbar.CollapsingToolbarLayout
@@ -29,10 +33,13 @@ class ClickedArtistFragment : Fragment() {
     private val binding get() = _binding!!
 
     private lateinit var playerModel: PlayerViewModel
+    private lateinit var favoriteModel : FavoriteViewModel
 
     private val args : ClickedArtistFragmentArgs by navArgs()
 
     val preferencesManager = PreferencesManager.getInstance()
+
+    private var isFavorite : Boolean = true
 
     private val clickedArtistAdapter: ClickedArtistTracksAdapter by lazy {
         ClickedArtistTracksAdapter(onItemClick = { playerModel.playTrack(it, QueueConstructor.IN_ARTIST.toInt()) })
@@ -44,7 +51,7 @@ class ClickedArtistFragment : Fragment() {
     ): View {
         _binding = FragmentClickedArtistBinding.inflate(inflater, container, false)
         playerModel = ViewModelProvider(this).get(PlayerViewModel::class.java)
-
+        favoriteModel = ViewModelProvider(this).get(FavoriteViewModel::class.java)
         val currentArtist = MusicStore.getInstance().artists.find { it.id == args.artistId }!!
 
         binding.artist = currentArtist
@@ -56,6 +63,30 @@ class ClickedArtistFragment : Fragment() {
                 activity?.window?.statusBarColor = ContextCompat.getColor(requireContext(), R.color.accent)
             }
         })
+
+        currentArtist.tracks.forEach { track ->
+            favoriteModel.MusicExist(track.id).observe(viewLifecycleOwner, Observer { item ->
+                if(item == null){
+                    Toast.makeText(activity, "BRAK w ulubionych", Toast.LENGTH_SHORT).show()
+                    isFavorite = false
+                    binding.toolbar.menu.getItem(0).setIcon(ContextCompat.getDrawable(requireContext(), R.drawable.ic_heart_outline))
+                }
+            })
+
+            if(isFavorite == false){
+                return@forEach
+            }
+        }
+
+        if(isFavorite){
+            Toast.makeText(activity, "ulubione", Toast.LENGTH_SHORT).show()
+            binding.toolbar.menu.getItem(0).setIcon(ContextCompat.getDrawable(requireContext(), R.drawable.ic_heart_filled))
+        }
+        else{
+            Toast.makeText(activity, "nie ulubione", Toast.LENGTH_SHORT).show()
+            binding.toolbar.menu.getItem(0).setIcon(ContextCompat.getDrawable(requireContext(), R.drawable.ic_heart_outline))
+        }
+
 
         binding.toolbar.setNavigationOnClickListener {
             activity?.window?.statusBarColor = ContextCompat.getColor(requireContext(), R.color.aboveBackground)
@@ -95,6 +126,9 @@ class ClickedArtistFragment : Fragment() {
             })
 
             playerModel.playArtist(currentArtist, true)
+
+
+
         }
 
         return binding.root
